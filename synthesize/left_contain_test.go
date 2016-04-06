b6926b3f058eed6f893e47f8a500e0744b4f6bce
Ken Grønnbeck <kengroenn@gmail.com>
2016-04-06 09:11:53 +0200

    Move remaining leftContain tests to left_contain_test.go

--- synthesize/left_contain_test.go
+++ synthesize/left_contain_test.go
@@ -1,6 +1,9 @@
 package synthesize
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func Test_leftContains_simple_true(t *testing.T) {
 	l := map[string]interface{}{}
@@ -137,5 +140,93 @@ func Test_leftContains_complex_array(t *testing.T) {
 	if !contains {
 		t.Log("Expected complex arrays to work but failed")
 		t.Fail()
+	}
+}
+
+func Test_ExpectedResponse_Comparison(t *testing.T) {
+	input := `{"hello": "world", "integer": 4, "float": 3.5}`
+
+	right := map[string]interface{}{}
+	err := json.Unmarshal([]byte(input), &right)
+
+	if err != nil {
+		t.Fatalf("Should be able to parse json but it failed")
+	}
+
+	left := map[string]interface{}{
+		"hello":   "world",
+		"integer": 4.0,
+		"float":   3.5,
+	}
+
+	contains := leftContains(left, right)
+
+	if !contains {
+		t.Fatal("Left should be contained in right")
+	}
+}
+
+func Test_ExpectedResponse_Comparison_Complex(t *testing.T) {
+	input := `{"complex": {"hello": "world"}}`
+
+	right := map[string]interface{}{}
+	err := json.Unmarshal([]byte(input), &right)
+
+	if err != nil {
+		t.Fatalf("Should be able to parse json but it failed")
+	}
+
+	left := map[string]interface{}{
+		"complex": map[string]interface{}{
+			"hello": "world",
+		},
+	}
+
+	contains := leftContains(left, right)
+
+	if !contains {
+		t.Fatal("Left should be contained in right")
+	}
+}
+
+func Test_ExpectedResponse_Comparison_Array(t *testing.T) {
+	input := `{"array": [1, 2, 3]}`
+
+	right := map[string]interface{}{}
+	err := json.Unmarshal([]byte(input), &right)
+
+	if err != nil {
+		t.Fatalf("Should be able to parse json but it failed")
+	}
+
+	left := map[string]interface{}{
+		"array": []interface{}{1.0, 2.0, 3.0},
+	}
+
+	contains := leftContains(left, right)
+
+	if !contains {
+		t.Fatal("Left should be contained in right")
+	}
+}
+
+func Test_ExpectedResponse_Comparison_NotEqual(t *testing.T) {
+	input := `{"hello2": "world"}`
+
+	right := map[string]interface{}{}
+	err := json.Unmarshal([]byte(input), &right)
+
+	if err != nil {
+		t.Fatalf("Should be able to parse json but it failed")
+	}
+
+	left := map[string]interface{}{
+		"hello": "world",
+	}
+
+	contains := leftContains(left, right)
+
+	if contains {
+		t.Fatal("Left should not be contained in right")
 	}
 }
